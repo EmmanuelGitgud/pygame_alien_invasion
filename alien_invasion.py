b86ce7fe7a26c4a6acb56344eb5084d13b6f35eb
EmmanuelGitgud <arabitemmanuel5@gmail.com>
2022-06-22 02:18:34 +0800

    Revert "Create bullet.py"
    This reverts commit 8f35ad6af2c9430bce3d60b8c1412425cd897377.

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,7 +31,12 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.ship.update()
-            self.bullets.update
+            self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
 
     def _check_events(self):
         for event in pygame.event.get():
